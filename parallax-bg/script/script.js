53520122a7e84840e2a758d2037fcb7f07949e56
Tan <ngoctana1k2014@gmail.com>
2023-08-14 00:15:11 +0700

    update login scoll

--- parallax-bg/script/script.js
+++ parallax-bg/script/script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 800);
 const CANVAS_HEIGHT = (canvas.height = 700);
-let gameSpeed = 5;
+let gameSpeed = 15;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'images/layer-1.png';
@@ -15,8 +15,17 @@ backgroundLayer4.src = 'images/layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'images/layer-5.png';
 
+let x = 0;
+let x2 = 2400;
+
 function animate() {
-  ctx.drawImage(backgroundLayer1, 0, 0);
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  ctx.drawImage(backgroundLayer4, x, 0);
+  ctx.drawImage(backgroundLayer4, x2, 0);
+  if (x < -2400) x = 2400 + x2 - gameSpeed;
+  else x -= gameSpeed;
+  if (x2 < -2400) x2 = 2400 + x - gameSpeed;
+  else x2 -= gameSpeed;
   requestAnimationFrame(animate);
 }
 animate();
