b21e89712efe43f24bc958e69e1770a6f5bdcb78
Tan <ngoctana1k2014@gmail.com>
2023-08-15 03:29:29 +0700

    test enemy class

---
+++ npc-movement/script/script.js
@@ -0,0 +1,44 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = (canvas.width = 500);
+const CANVAS_HEIGHT = (canvas.height = 1000);
+const numberOfEnemies = 100;
+const enemiesArray = [];
+
+const enemy1 = {
+  x: 10,
+  y: 50,
+  width: 200,
+  height: 200,
+};
+
+class Enemy {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.width = 100;
+    this.height = 100;
+  }
+  update() {
+    this.x++;
+    this.y++;
+  }
+  draw() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemiesArray.push(new Enemy());
+}
+
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemiesArray.forEach((enemy) => {
+    enemy.update();
+    enemy.draw();
+  });
+  requestAnimationFrame(animate);
+}
+animate();
