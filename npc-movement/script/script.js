907be3e5ebd03fa8d1e434fd39c9f8c8907dd327
Tan <ngoctana1k2014@gmail.com>
2023-08-16 16:43:38 +0700

    done enemy1

--- npc-movement/script/script.js
+++ npc-movement/script/script.js
@@ -7,32 +7,32 @@ const numberOfEnemies = 100;
 /** @type {Enemy[]} */
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = 'images/enemy1.png';
 let gameFrame = 0;
 
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.speed = Math.random() * 4 - 2;
+    this.image = new Image();
+    this.image.src = 'images/enemy1.png';
+    // this.speed = Math.random() * 4 - 2;
     this.spriteWidth = 293;
     this.spriteHeight = 155;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
-    if (gameFrame % 2 === 0) {
+    this.x += Math.random() * 15 - 7.5;
+    this.y += Math.random() * 10 - 5;
+    if (gameFrame % this.flapSpeed === 0) {
       this.frame > 4 ? (this.frame = 0) : this.frame++;
     }
   }
   draw() {
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
-      enemyImage,
+      this.image,
       this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
