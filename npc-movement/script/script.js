ed24c4b6ac6e7130a7f3f22704d490e233845dcb
Tan <ngoctana1k2014@gmail.com>
2023-08-16 16:25:07 +0700

    update enemy move logic

--- npc-movement/script/script.js
+++ npc-movement/script/script.js
@@ -4,29 +4,36 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = (canvas.width = 500);
 const CANVAS_HEIGHT = (canvas.height = 1000);
 const numberOfEnemies = 100;
+/** @type {Enemy[]} */
 const enemiesArray = [];
 
 const enemyImage = new Image();
 enemyImage.src = 'images/enemy1.png';
+let gameFrame = 0;
 
 class Enemy {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
     this.speed = Math.random() * 4 - 2;
     this.spriteWidth = 293;
     this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.frame = 0;
   }
   update() {
     this.x += this.speed;
     this.y += this.speed;
+    if (gameFrame % 2 === 0) {
+      this.frame > 4 ? (this.frame = 0) : this.frame++;
+    }
   }
   draw() {
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
     ctx.drawImage(
       enemyImage,
-      0,
+      this.frame * this.spriteWidth,
       0,
       this.spriteWidth,
       this.spriteHeight,
@@ -48,6 +55,7 @@ function animate() {
     enemy.update();
     enemy.draw();
   });
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
