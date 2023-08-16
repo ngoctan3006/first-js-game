3457c6345af555516f648b4fb00253408bd7a23a
Tan <ngoctana1k2014@gmail.com>
2023-08-16 17:48:35 +0700

    done enemy3

--- npc-movement/script/script.js
+++ npc-movement/script/script.js
@@ -12,23 +12,29 @@ let gameFrame = 0;
 class Enemy {
   constructor() {
     this.image = new Image();
-    this.image.src = 'images/enemy2.png';
+    this.image.src = 'images/enemy3.png';
     this.speed = Math.random() * 4 + 1;
-    this.spriteWidth = 266;
-    this.spriteHeight = 188;
+    this.spriteWidth = 218;
+    this.spriteHeight = 177;
     this.width = this.spriteWidth / 2;
     this.height = this.spriteHeight / 2;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = Math.random() * 2;
-    this.angleSpeed = Math.random() * 0.2;
-    this.curve = Math.random() * 10;
+    this.angle = 0;
+    this.angleSpeed = Math.random() * 1.5 + 0.5;
+    // this.curve = Math.random() * 200 + 50;
   }
   update() {
-    this.x -= this.speed;
-    this.y += this.curve * Math.sin(this.angle);
+    this.x =
+      (canvas.width / 2) * Math.cos((this.angle * Math.PI) / 90) +
+      canvas.width / 2 -
+      this.width / 2;
+    this.y =
+      (canvas.height / 2) * Math.sin((this.angle * Math.PI) / 270) +
+      canvas.height / 2 -
+      this.height / 2;
     this.angle += this.angleSpeed;
     if (this.x + this.width < 0) this.x = canvas.width;
     if (gameFrame % this.flapSpeed === 0) {
