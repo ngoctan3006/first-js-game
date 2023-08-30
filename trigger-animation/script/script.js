295d9a3a93f65d6c775dc08f59ff32aeeb02104d
Tan <ngoctana1k2014@gmail.com>
2023-08-31 00:56:15 +0700

    add boom animation

--- trigger-animation/script/script.js
+++ trigger-animation/script/script.js
@@ -2,3 +2,57 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = 500;
 canvas.height = 700;
+const explosions = [];
+let canvasPosition = canvas.getBoundingClientRect();
+
+class Explosion {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.width = this.spriteWidth * 0.7;
+    this.height = this.spriteHeight * 0.7;
+    this.image = new Image();
+    this.image.src = 'images/boom.png';
+    this.frame = 0;
+    this.timer = 0;
+  }
+
+  update() {
+    this.timer++;
+    if (this.timer % 10 === 0) {
+      this.frame++;
+    }
+  }
+
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.spriteWidth * this.frame,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+window.addEventListener('click', (e) => {
+  let positionX = e.x - canvasPosition.left;
+  let positionY = e.y - canvasPosition.top;
+  explosions.push(new Explosion(positionX, positionY));
+});
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].update();
+    explosions[i].draw();
+  }
+  requestAnimationFrame(animate);
+}
+animate();
