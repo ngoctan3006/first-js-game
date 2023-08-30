7db557d4404504264308b93f903dfe4912fe7118
Tan <ngoctana1k2014@gmail.com>
2023-08-31 01:22:55 +0700

    rotate

--- trigger-animation/script/script.js
+++ trigger-animation/script/script.js
@@ -11,12 +11,13 @@ class Explosion {
     this.spriteHeight = 179;
     this.width = this.spriteWidth * 0.7;
     this.height = this.spriteHeight * 0.7;
-    this.x = x - this.width / 2;
-    this.y = y - this.height / 2;
+    this.x = x;
+    this.y = y;
     this.image = new Image();
     this.image.src = 'images/boom.png';
     this.frame = 0;
     this.timer = 0;
+    this.angle = Math.random() * 6.2;
   }
 
   update() {
@@ -27,17 +28,21 @@ class Explosion {
   }
 
   draw() {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
     ctx.drawImage(
       this.image,
       this.spriteWidth * this.frame,
       0,
       this.spriteWidth,
       this.spriteHeight,
-      this.x,
-      this.y,
+      0 - this.width / 2,
+      0 - this.height / 2,
       this.width,
       this.height
     );
+    ctx.restore();
   }
 }
 
