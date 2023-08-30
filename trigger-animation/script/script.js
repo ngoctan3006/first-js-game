554b23edd46f5509bc2883023cf0d412a49b91d1
Tan <ngoctana1k2014@gmail.com>
2023-08-31 01:02:50 +0700

    refactor

--- trigger-animation/script/script.js
+++ trigger-animation/script/script.js
@@ -42,10 +42,14 @@ class Explosion {
 }
 
 window.addEventListener('click', (e) => {
+  createAnimation(e);
+});
+
+function createAnimation(e) {
   let positionX = e.x - canvasPosition.left;
   let positionY = e.y - canvasPosition.top;
   explosions.push(new Explosion(positionX, positionY));
-});
+}
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
